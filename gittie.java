ef07f1330a557381eb2719074c9ef1504f68fd6e
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-01-11 09:39:30 +0100

    joke method implemented

--- gittie.java
+++ gittie.java
@@ -2,8 +2,13 @@ public class gittie {
     public static void introduce() {
         System.out.println("Hello, I'm Gittie!");
     }
+    public static void joke() {
+        System.out.println("\n\nTudod mi jó még?!");
+        System.out.println("\n\n\n A faszom!wühühühü");
+    }
     public static void main(String[] args) {
         introduce();
+        joke();
         }
     }
             
