6686467eba9b86e3f80fffe4e8888528166d3b7f
szepecsikzsolt <swuprising2@gmail.com>
2018-01-11 09:42:11 +0100

    test

--- gittie.java
+++ gittie.java
@@ -1,9 +1,13 @@
 public class gittie {
     public static void introduce() {
         System.out.println("Hello, I'm Gittie!");
+        }
+    public static void bye() {
+        System.out.println("See you next time!");
     }
     public static void main(String[] args) {
-        introduce();
+        introduce();   
+        bye();
         }
     }
             
