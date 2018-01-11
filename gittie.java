fa4f0391c2ae52b7c7dfdec482f7a5889ebd1c32
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-01-11 09:23:08 +0100

    gittie created

---
+++ gittie.java
@@ -0,0 +1,9 @@
+public class gittie {
+    public static void Main(String[] args) {
+
+        public void introduce() {
+            System.out.println("Hello, I'm Gittie!")
+        }
+    }
+    
+}
