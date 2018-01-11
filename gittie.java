416eac6177ca47ce312e0cd01570d87630e6941a
GaborMartin <gabormartin94@gmail.com>
2018-01-11 09:39:41 +0100

    Implemented add method.

--- gittie.java
+++ gittie.java
@@ -2,9 +2,16 @@ public class gittie {
     public static void introduce() {
         System.out.println("Hello, I'm Gittie!");
     }
+    public static void add(int a, int b) {
+        int result;
+        result = a + b;
+        System.out.println(result);
+    }
     public static void main(String[] args) {
         introduce();
+        add(5, 10);
         }
     }
+    
             
     
